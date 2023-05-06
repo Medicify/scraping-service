d655e295f7102fd0be2274fd2f14e8959f39953f
saddam-satria <ardhisaddam@gmail.com>
2023-05-06 17:45:43 +0700

    init

---
+++ main.py
@@ -0,0 +1,59 @@
+import requests 
+from bs4 import BeautifulSoup
+import time
+
+urls = open("urls.txt", "r").read().split("\n")
+
+BASE_URL = "https://www.halodoc.com/obat-dan-vitamin"
+
+detail_products = []
+drugs = []
+
+
+def main():
+    print("scraping....")
+    start_time = time.time()
+    for url in urls:
+        result = requests.get(url)
+        soup = BeautifulSoup(result.text, "html.parser")
+        # titles = soup.find_all("div", class_="ngx-ellipsis-inner")
+        li_elements =soup.find_all("li", class_="custom-container__list__container__item")
+        # detail_product = li_elements[0].find("a", class_="custom-container__list__container__item--link")
+
+        for li_element in li_elements:
+            detail_product = li_element.find("a", class_="custom-container__list__container__item--link")
+            detail_products.append(detail_product['href'])
+
+
+    for detail in detail_products:
+        get_detail_products = requests.get(BASE_URL + detail)
+        bs = BeautifulSoup(get_detail_products.text, "html.parser")
+        
+        images = bs.find("img",class_="hd-base-image-mapper__img product-image")
+        titles = bs.find("h1",class_="product-label")
+        drug_details = bs.find_all("div", class_="drug-detail")
+        detail_titles = bs.find_all("div", class_="ttl-list")
+
+
+        detail_drug = {}
+        for index in range(len(detail_titles)):
+            detail_drug[detail_titles[index].text.lower()] = "-" if drug_details[index].find('div').text == "" else drug_details[index].find('div').text.replace("\n", "")
+           
+
+        
+        drugs.append({
+            "image" : images["src"], 
+            "title" : titles.text,
+            "detail" : detail_drug
+        })
+       
+
+
+    print(f"execution time: {(time.time() - start_time)}" )
+    time.sleep(1)
+    print(f'total data scrape: {len(drugs)}')
+
+if __name__ == "__main__":
+    main()
+
+    
